ea52d1487154d8737af3567df7fb471db3e3a213
PedroacBraz <pedrobraz@comp.ufu.br>
2016-11-24 18:28:12 -0200

    bubble

--- Sorts/Sorts/Program.cs
+++ Sorts/Sorts/Program.cs
@@ -8,8 +8,56 @@ namespace Sorts
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public static void bubbleSort(int[] arr) {
+
+            int temp = 0;
+
+            for (int write = 0; write < arr.Length; write++)
+            {
+                for (int sort = 0; sort < arr.Length - 1; sort++)
+                {
+                    if (arr[sort] > arr[sort + 1])
+                    {
+                        temp = arr[sort + 1];
+                        arr[sort + 1] = arr[sort];
+                        arr[sort] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+
+            Console.ReadKey();
+
+
+        }
+
+
+
+        public static void Main(string[] args)
         {
+            int[] arr = { 800, 11, 50, 771, 649, 770, 240, 9 };
+
+
+            int sort;
+
+
+            Console.WriteLine("Informe o sort que deseja utilizar: ");
+            sort = int.Parse(Console.ReadLine());
+
+            switch (sort)
+            {
+                case 1:
+                    bubbleSort(arr);                   
+                    break;
+
+
+                default:
+                    Console.Write("Opção Inválida!");
+                    break;
+            }
         }
     }
 }
